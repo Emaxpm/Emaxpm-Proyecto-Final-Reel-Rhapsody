2cd7acd2907fbc459d91b22bd74b201ca76e04ec
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-12-14 01:03:57 +0000

    agregue estilos al body

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -12,12 +12,12 @@ export const Card = () => {
     return (
         <div className="card-container">
             {store.films.map((item => <div key={item.id} className="card">
-                    <img src={'https://image.tmdb.org/t/p/w500' + item.backdrop_path} className="d-block w-100" alt="..." />
+                    <img src={'https://image.tmdb.org/t/p/w500' + item.backdrop_path} className="pic" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{item.original_title}</h5>
                         <p className="card-text">Release Date: {item.release_date}</p>
                         <p className="card-text">vote: {item.vote_average}</p>
-                        <p className="card-text">Id: {item.id}</p>
+                        {/* <p className="card-text">Id: {item.id}</p> */}
                         {/* <div>
                             <Link to={`/character/${index+1}`} className="btn btn-outline-dark me-3">Learn more!</Link>
                             <button className="btn btn-outline-warning" to="#" ><i className="fa-regular fa-heart" onClick={()=>{
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,33 +9,13 @@ export const Home = () => {
 		<div>
 			<img className="presentation" src={imageCinema}/>
 		</div>
-		<div className="container-fluid bg-black">
-			<div className="accordion">
-				<div className="accordion-item bg-black">
-					<h2 className="accordion-header">
-						<button className="accordion-button collapsed title" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-							MOVIES
-						</button>
-					</h2>
-					<div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-						<div className="accordion-body"><Card/></div>
-					</div>
-				</div>			
-			</div>		
+		<div className="container-fluid row">
+			<h2 className="col-12 title">MOVIES</h2>
+			<div><Card/></div>			
 		</div>
-		<div className="container-fluid bg-black">
-			<div className="accordion">
-				<div className="accordion-item bg-black">
-					<h2 className="accordion-header">
-						<button className="accordion-button collapsed title" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-							SERIES
-						</button>
-					</h2>
-					<div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-						<div className="accordion-body"><Card/></div>
-					</div>
-				</div>			
-			</div>		
+		<div className="container-fluid row">
+			<h2 className="col-12 title">SERIES</h2>
+			<div><Card/></div>
 		</div>
 	</div>
 	);
