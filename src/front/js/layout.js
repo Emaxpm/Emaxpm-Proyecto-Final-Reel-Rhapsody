c0ccb746bac48b2dcd3c2686f6ae784b277f8b0f
JancarlosT <138736732+JancarlosT@users.noreply.github.com>
2023-12-19 15:20:57 +0000

    payment

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import Payment  from "./pages/payment.js";
 import Signup from "./component/SignUp.jsx";
 import LogIn from "./component/LogIn.jsx";
 
@@ -29,6 +30,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<LogIn />} path="/" />
                         <Route element={<Home />} path="/home" />
+                        <Route element={<Payment />} path="/payment" />
                         <Route element={<Signup />} path="signup" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
