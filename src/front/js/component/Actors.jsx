9409040cd91ffc4659ee336eb7ca6474c30ba0f3
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-12-21 03:34:06 +0000

    agregue grid al vi yeestilos w de Actors

--- src/front/js/component/Actors.jsx
+++ src/front/js/component/Actors.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
-import "../../styles/card.css";
+import "../../styles/actors.css";
 
 const Actors = () => {
     const { store, actions } = useContext(Context)
@@ -11,14 +11,43 @@ const Actors = () => {
     console.log(store.actor)
     return (
         <div className="card-container">
-            {store.actor.map((item => <div key={item.id} className="card">
-                    <img src={'https://image.tmdb.org/t/p/w500' + item.profile_path} className="pic" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{item.name}</h5>
-                        <p className="card-text">vote: {item.popularity}</p>                       
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col-3">
+                        {store.actor.slice(0, Math.ceil(store.actor.length / 3)).map((item) => (
+                            <div key={item.id} className="card">
+                                <img src={'https://image.tmdb.org/t/p/w500' + item.profile_path} className="pic" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.name}</h5>
+                                    <p className="card-text">vote: {item.popularity}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="col-3">
+                        {store.actor.slice(Math.ceil(store.actor.length / 3), Math.ceil(store.actor.length * 2 / 3)).map((item) => (
+                            <div key={item.id} className="card">
+                                <img src={'https://image.tmdb.org/t/p/w500' + item.profile_path} className="pic" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.name}</h5>
+                                    <p className="card-text">vote: {item.popularity}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="col-3">
+                        {store.actor.slice(Math.ceil(store.actor.length * 2 / 3)).map((item) => (
+                            <div key={item.id} className="card">
+                                <img src={'https://image.tmdb.org/t/p/w500' + item.profile_path} className="pic" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.name}</h5>
+                                    <p className="card-text">vote: {item.popularity}</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
-            ))}           
+            </div>
         </div>
     );
 };
