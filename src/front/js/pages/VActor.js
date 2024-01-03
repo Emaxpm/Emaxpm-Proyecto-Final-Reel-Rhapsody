466901ffd25ebf471833479b85faf69eb413d16f
Emaxpm <140901128+Emaxpm@users.noreply.github.com>
2024-01-03 04:11:17 +0000

    agregue estilos al home e hice un models de movies y series

--- src/front/js/pages/VActor.js
+++ src/front/js/pages/VActor.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import DetailActor from "../component/DetailActor.jsx";
 
-import { Context } from "../store/appContext";
-
 export const VActor = () => {
 	return (
 		<div className="container">
-			<div className="container-fluid row">
-				<h2 className="col-12 title">Actor</h2>
+			<div className="">
+				<h2 className="title">Actor</h2>
 				<div><DetailActor/></div>
 			</div>
 			<br />
