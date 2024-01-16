a2d995c8306c16a743365abd3efd2f17c775bac4
Pamela <140130365+Pametr88@users.noreply.github.com>
2024-01-16 02:18:32 +0000

    agregue estilos a footer y sus componentes

--- src/front/js/component/Footer.js
+++ src/front/js/component/Footer.js
@@ -14,11 +14,13 @@ const Footer = () => {
 
                 <div className="foot-info">
 
-                    <p className="mx-1 p-f" role="button" onClick={() => navigate("/payment")}>Donatios</p>
+                    <p className="mx-1 p-f button-r" role="button" onClick={() => navigate("/technicalsupport")}>Technical Support</p>
 
-                    <p className="mx-1 p-f" role="button" onClick={() => navigate("/technicalsupport")}>Technical Support</p>
+                    <p className="mx-1 p-f button-r" role="button" onClick={() => navigate("/payment")}>Donatios</p>
 
-                    <p className="mx-1 p-f" role="button" onClick={() => navigate("/preguntas")}>Frequent Questions</p>
+                    <p className="mx-1 p-f button-r" role="button" onClick={() => navigate("/preguntas")}>Frequent Questions</p>
+
+                    <p className="mx-1 p-f button-r" role="button" onClick={() => navigate("/aboutus")}>About Us</p>
 
                 </div>
 
